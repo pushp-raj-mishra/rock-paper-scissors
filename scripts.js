154c72be12cf19c8bf27e27795334468b8172516
PUSHP RAJ <he.is.pushp@gmail.com>
2025-01-05 03:48:59 +0000

    complete getComputerChoice and getUserChoice

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function getComupterChoice() {
+    let x = Math.floor(Math.random() * 100) + 1;
+    if (x <= 33) {
+        return "rock";
+    } else if (x <= 66) {
+        return "paper";
+    } else {
+        return "scissor";
+    }
+}
+
+function getUserChoice() {
+    return prompt("Enter Your Choice: (rock, paper, scissor)");
+}
+
+console.log(getUserChoice());
