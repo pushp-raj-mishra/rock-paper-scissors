f2d1bc7ae1e37c12946735a3472b08dbcbd95272
PUSHP RAJ <he.is.pushp@gmail.com>
2025-01-08 05:46:05 +0000

    integrate js with UI

--- scripts.js
+++ scripts.js
@@ -9,42 +9,70 @@ function getComupterChoice() {
     }
 }
 
-function getUserChoice() {
-    return prompt("Enter Your Choice: (rock, paper, scissor)");
-}
+
+let userChoice = '';
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+const verdict = document.querySelector(".verdictSpan");
+const uScore = document.querySelector(".userScore");
+const cScore = document.querySelector(".computerScore");
+
+rock.addEventListener("click",function() {
+    userChoice = 'rock';
+    playRound();
+})
+
+paper.addEventListener("click",function() {
+    userChoice = 'paper';
+    playRound();
+})
+
+scissor.addEventListener("click",function() {
+    userChoice = 'scissor';
+    playRound();
+})
+
 
 function playRound() {
-    let userChoice = getUserChoice().toLowerCase();
+    
     let computerChoice = getComupterChoice();
     if (userChoice == computerChoice) {
+        verdict.textContent = "Tie";
         console.log("Tie");
         userScore++;
         computerScore++;
+        uScore.textContent = `User Score: ${userScore}`;
+        cScore.textContent = `Computer Score: ${computerScore}`;
     } else if (((userChoice=="rock")&&(computerChoice=="paper")) || ((userChoice=="paper")&&(computerChoice=="scissor")) || ((userChoice=="scissor")&&(computerChoice=="rock"))) {
+        verdict.textContent = `You Lose! ${computerChoice} beats ${userChoice}`;
         console.log(`You Lose! ${computerChoice} beats ${userChoice}`);
         computerScore++;
+        uScore.textContent = `User Score: ${userScore}`;
+        cScore.textContent = `Computer Score: ${computerScore}`;
     } else {
+        verdict.textContent = `You Win! ${userChoice} beats ${computerChoice}`;
         console.log(`You Win! ${userChoice} beats ${computerChoice}`);
         userScore++;
+        uScore.textContent = `User Score: ${userScore}`;
+        cScore.textContent = `Computer Score: ${computerScore}`;
+    }
+    if ((userScore==5)||(computerScore==5)) {
+        if (userScore>computerScore) {
+            verdict.textContent = "Game Over!! You Win!!";
+        } else if (computerScore>userScore) {
+            verdict.textContent = "Game Over!! Computer Wins!!";
+        } else {
+            verdict.textContent = "Game Over!! It was a tie!!";
+        }
+        userScore = 0;
+        computerScore = 0;
     }
 }
 
 let userScore = 0;
 let computerScore = 0;
 
-function playGame(){
-    for(let i = 0; i < 5; i++){
-        playRound();
-    }
-    console.log(`User: ${userScore}`);
-    console.log(`Computer: ${computerScore}`);
-    if (userScore==computerScore) {
-        console.log(`Tie`);
-    } else if (userScore>computerScore) {
-        console.log(`You Win`);
-    } else {
-        console.log(`Computer Wins`);
-    }
-}
 
-playGame();
+
+
